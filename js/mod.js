afaaa374e3f830a14c11d68f7864b0098f9c7f64
Edward122403 <156387291+Edward122403@users.noreply.github.com>
2025-04-27 11:35:07 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,79 +1,39 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
-	modFiles: ["layers.js", "tree.js"],
+    name: "The Tree Tree",
+    id: "treeTree",
+    author: "Edward1224",
+    pointsName: "Tree Points",
+    modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
-}
+    initialStartPoints: new Decimal(0),
+    offlineLimit: 1, // In hours
+};
 
-// Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+    num: "0.0.1",
+    name: "The First Tree",
+};
+
+let changelog = `<h1>Changelog:</h1><br><br>v0.0.1 - Starter Kit`;
+
+let winText = `Congratulations! You've beaten all the trees... for now!`;
+
+function getStartPoints() {
+    return new Decimal(0);
 }
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
-
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
-
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
-
-function getStartPoints(){
-    return new Decimal(modInfo.initialStartPoints)
+function canGenPoints() {
+    return false; // No passive Tree Points generation
 }
 
-// Determines if it should show points/sec
-function canGenPoints(){
-	return true
+function gainTreePoints(layer) {
+    let gain = new Decimal(1);
+    player.points = player.points.add(gain);
 }
 
-// Calculate points/sec!
-function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
-
-	let gain = new Decimal(1)
-	return gain
-}
-
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
-
-// Display extra things at the top of the page
-var displayThings = [
-]
-
-// Determines when the game "ends"
-function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
-}
-
-
-
-// Less important things beyond this point!
-
-// Style for the background, can be a function
-var backgroundStyle = {
-
-}
-
-// You can change this if you have things that can be messed up by long tick lengths
-function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
-}
-
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function isLayerVisible(layer) {
+    if (!player[layer]) return false;
+    if (layer === "tp") return true;
+    if (player.currentSubtree && player.currentSubtree !== layer) return false;
+    return player[layer].unlocked;
 }
